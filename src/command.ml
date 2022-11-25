66c018458da5a50a388ed3467878ddd992a68ce2
svs45 <svs45@cornell.edu>
2022-11-25 18:25:38 -0500

    Made changes to command.ml

--- src/command.ml
+++ src/command.ml
@@ -1,10 +1,19 @@
-type move =
-  | Up
-  | Down
-  | Left
-  | Right
+type input_recieved = {
+  key_pressed : char;
+  mouse_pressed : bool;
+}
 
-type t =
-  | Move of move
-  | Quit
-  | Pause
+type t = {
+  key_current : char;
+  steps : int;
+}
+
+let starting_input () = { key_current = Char.chr 0; steps = 0 }
+
+let new_frame (frame : Graphics.status) =
+  { key_pressed = frame.key; mouse_pressed = frame.button }
+
+let get_input () =
+  if Graphics.key_pressed () then
+    Graphics.wait_next_event [ Graphics.Key_pressed ]
+  else Graphics.wait_next_event [ Graphics.Poll ]
