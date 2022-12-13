5390edde8f7eb9df243094daed09371b6049f7df
svs45 <svs45@cornell.edu>
2022-12-13 01:48:18 -0500

    Gets rid of current error in test cases

--- test/main.ml
+++ test/main.ml
@@ -5,17 +5,7 @@ open Characters
 let check_test (name : string) (input : int) (output : int) : test =
   name >:: fun _ -> assert_equal input output ~printer:string_of_int
 
-let check_state (name : string) input output : test =
-  name >:: fun _ -> assert_equal input output
-
-let check_tests =
-  [
-    check_test "basic check test" (4 + 6) 10;
-    check_state "checking states"
-      { location = (0, 0); object_type = Tree; speed = 0 }
-      { location = (0, 0); object_type = Tree; speed = 0 };
-  ]
-
+let check_tests = [ check_test "basic check test" (4 + 6) 10 ]
 let gui_tests = []
 let state_tests = []
 
