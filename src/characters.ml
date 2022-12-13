7de085b412072d8aee9e81935bb6377adf11b9e5
Sanjana <sk2465@cornell.edu>
2022-12-13 10:11:52 -0500

    Added car object to characters

--- src/characters.ml
+++ src/characters.ml
@@ -17,11 +17,25 @@ type obstacle = {
   location : int * int;
 }
 
+type moving =
+  | Log
+  | Car
+
+type moving_ob = {
+  ob_type : moving;
+  mutable location : int * int;
+  speed : int;
+  frame : int;
+}
+
 let trees (x : int) (y : int) =
   { object_type = Tree; location = (Random.int 1000 + x, Random.int 150 + y) }
 
 let rock (x : int) (y : int) =
   { object_type = Rock; location = (Random.int 1000 + x, Random.int 150 + y) }
+
+let car (ty : moving) (x : int) (y : int) (s : int) (f : int) =
+  { ob_type = Car; location = (x, y); speed = s; frame = f }
 
 let draw_oompa (t : player) x y =
   let a = t.location = (x, y) in
