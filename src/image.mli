66ea670549b7c55ddec8fada58f95b4507bcaba3
svs45 <svs45@cornell.edu>
2022-12-14 18:57:54 -0500

    Changes to image.mli

--- src/image.mli
+++ src/image.mli
@@ -10,13 +10,8 @@
     source:
     https://gitlab.com/camlspotter/camlimages/-/blob/hg-b4.2.6/src/graphic_image.ml *)
 
-<<<<<<< HEAD
 val array_of_image : 'a -> 'b -> 'c -> 'd -> 'e
 (*[array_of_image] converts the image into an a color matrix. *)
-=======
-val array_of_image : 'a -> 'b -> 'c -> 'd -> 'weak938
-(** [array_of_image] converts the image into an a color matrix. *)
->>>>>>> 3caa3193902e395d9ca613b2018db666120a65de
 
 val of_image : 'a -> 'b -> 'c -> 'd -> 'e
 (** [of_image] uses the Graphics function [make_image] to convert the color
