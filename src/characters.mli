29e0b9773b2dba97aee02a0eae731a522ee5b1fa
svs45 <svs45@cornell.edu>
2022-12-14 16:54:55 -0500

    Changed .mli specs to be underneath function not above

--- src/characters.mli
+++ src/characters.mli
@@ -1,13 +1,11 @@
-(*Representation of the different "characters" in the game.
+(** Representation of the different "characters" in the game.
 
-  This module outlines the different types of objects in the game, including
-  player, obj (non-moving obstacles), and moving (moving obstacles). Each of
-  these has its own types that represent different objects.
+    This module outlines the different types of objects in the game, including
+    player, obj (non-moving obstacles), and moving (moving obstacles). Each of
+    these has its own types that represent different objects. *)
 
-  *****************************************************************************)
+(*****************************************************************************)
 
-(*The player type represents the record fields of the main player, which include
-  location, speed, frame, steps, width, and height*)
 type player = {
   mutable location : int * int;
   speed : int;
@@ -16,32 +14,34 @@ type player = {
   oompa_width : int;
   oompa_height : int;
 }
+(*The player type represents the record fields of the main player, which include
+  location, speed, frame, steps, width, and height*)
 
-(*The obj type represents the non-moving objects, which include the trees, the
-  rocks, and the water*)
 type obj =
   | Tree
   | Rock
   | Water
+(*The obj type represents the non-moving objects, which include the trees, the
+  rocks, and the water*)
 
-(*The obstacle type represents the non-moving obstacles of the game and it also
-  includes a record field defining their location*)
 type obstacle = {
   object_type : obj;
   location : int * int;
 }
+(*The obstacle type represents the non-moving obstacles of the game and it also
+  includes a record field defining their location*)
 
-(*The moving type represents the moving objects/obstacles. In this game, it is a
-  car*)
 type moving =
   | Log
   | Car
+(*The moving type represents the moving objects/obstacles. In this game, it is a
+  car*)
 
-(*The direction type represents the direction the cars move in. Right is moving
-  in the positive x direction and Left is moving in the negative x direction*)
 type direction =
   | Right
   | Left
+(*The direction type represents the direction the cars move in. Right is moving
+  in the positive x direction and Left is moving in the negative x direction*)
 
 (*The moving_ob type defines record fields location, time, speed, frame, and
   direction for the moving objects*)
@@ -53,80 +53,82 @@ type moving_ob = {
   frame : int;
   direction : direction;
 }
+(*The moving_ob type defines record fields location, time, speed, frame, and
+  direction for the moving objects*)
 
-(*The abstract type represents the main player, a list of moving objects, and
-  the game state*)
 type t = {
   oompa : player;
   characters_moving : moving_ob list;
   state : State.game_mode;
 }
+(*The abstract type represents the main player, a list of moving objects, and
+  the game state*)
 
-(*type e represents a Tick*)
 type e = Tick
+(*type e represents a Tick*)
 
+val car_walk_n : moving_ob -> unit
 (*[car_walk_n c] decrements the x-coordinate of the location of c by the speed
   of c*)
-val car_walk_n : moving_ob -> unit
 
+val check_coll : int * int -> moving_ob list -> moving_ob option
 (*[check_coll l cs] checks if the main player collides with a moving object. If
   it does, the object is returned. If not, None is returned*)
-val check_coll : int * int -> moving_ob list -> moving_ob option
 
-(*[update_game_state map] updates the map's field state with a new state*)
 val update_game_state : t -> t
+(*[update_game_state map] updates the map's field state with a new state*)
 
+val tick : t -> t
 (*[tick map] updates the location of the moving object as long as the state of
   map is set to Play. The x-coordinate is incremented by the speed*)
-val tick : t -> t
 
-(*[handle_event map event]*)
 val handle_event : t -> e -> t
+(*[handle_event map event]*)
 
+val spawn_moving_ob : int * int -> moving -> moving_ob
 (*[spawn_moving_ob l ob ] sets the record fields for a new moving object. l is
   the (x,y) coordinates for the location field. ob is the object that is
   spawned*)
-val spawn_moving_ob : int * int -> moving -> moving_ob
 
-(*car_list type represents a list of cars*)
 type car_list = { mutable hist_cars : moving_ob list }
+(*car_list type represents a list of cars*)
 
-(*[car_width] is the width of the car*)
 val car_width : int
+(*[car_width] is the width of the car*)
 
-(*[car_height] is the height of the car*)
 val car_height : int
+(*[car_height] is the height of the car*)
 
-(*[car_walk] is the width of the car divided by 4*)
 val car_walk : int
+(*[car_walk] is the width of the car divided by 4*)
 
+val trees : int -> int -> obstacle
 (*[trees x y] initializes an obstacle that is a tree with location set to be
   randomly generated coordinates with an x and y bound added to the respective
   coordinate*)
-val trees : int -> int -> obstacle
 
+val rock : int -> int -> obstacle
 (*[rocks x y] initializes an obstacle that is a rock with location set to be
   randomly generated coordinates with an x and y bound added to the respective
   coordinate*)
-val rock : int -> int -> obstacle
 
-(*[create_car_lst c] initializes a list of cars*)
 val create_car_lst : car_list -> car_list
+(*[create_car_lst c] initializes a list of cars*)
 
-(*[add_car c car] adds car to the list c.hist_cars*)
 val add_car : car_list -> moving_ob -> unit
+(*[add_car c car] adds car to the list c.hist_cars*)
 
+val car : moving -> int -> int -> int -> int -> int -> direction -> moving_ob
 (*[car type x y t s f d] initializes a car object with fields ob_type, location,
   time, speed, frame, and direction*)
-val car : moving -> int -> int -> int -> int -> int -> direction -> moving_ob
 
+val draw_car : moving_ob -> unit -> unit
 (*[draw_car c] draws a car at the x and y coordinates specified by its location
   field*)
-val draw_car : moving_ob -> unit -> unit
 
+val move_car : moving_ob -> moving_ob list -> int -> unit
 (*[move_car c hist] moves the car in the direction specified by its direction
   field. The car's x-coordinate is set to 0 if it drives out of frame*)
-val move_car : moving_ob -> moving_ob list -> int -> unit
 
-(*[updateCar c hist] calls [move_car c hist] to recursively move the car *)
 val updateCar : moving_ob -> moving_ob list -> int -> unit
+(*[updateCar c hist] calls [move_car c hist] to recursively move the car *)
--- src/state.mli
+++ src/state.mli
@@ -1,68 +1,64 @@
-(*Representation of the different states the game can be in. 
+(**Representation of the different states the game can be in.
 
-    This module outlines the different game modes, including start, play, pause
-    and fail. Each of these states has a set of actions (and screens) associated
-    with it that is displayed using this module.
-    
-*******************************************************************************)
+   This module outlines the different game modes, including start, play, pause
+   and fail. Each of these states has a set of actions (and screens) associated
+   with it that is displayed using this module.*)
 
+(******************************************************************************)
 
-(*The game_mode type consists of constructors for the various game states*)
 type game_mode =
   | Start
   | Play
   | Pause
   | Fail
-  | Win 
+  | Win
+(*The game_mode type consists of constructors for the various game states*)
 
-(*The images type represents the pnf images depicting the objects on the game 
-   board *)
 type images
+(*The images type represents the pnf images depicting the objects on the game
+  board *)
 
-
-
-(*The abstract type that represents the player's key and mouse inputs and a 
-   mutable field game_state that represents which mode the game is in (this 
-   has type game_mode)*)
 type t = {
   mutable game_state : game_mode;
   mouse_pressed : bool;
   arrow_pressed : bool;
 }
+(*The abstract type that represents the player's key and mouse inputs and a
+  mutable field game_state that represents which mode the game is in (this has
+  type game_mode*)
 
+val init_images : t
 (*[init_images] sets game_state to Start*)
-val init_images: t 
 
-(*[text s size color] defines the set string, size, and color for the text that
-   will be printed out on each screen*)
 val text : string -> int -> int -> unit
+(*[text s size color] defines the set string, size, and color for the text that
+  will be printed out on each screen*)
 
-(*[draw_start_screen] draws the start_screen when the record field game_state
-   is set to Start. This displays text asking for an input to start the game*)
 val draw_start_screen : unit -> unit
+(*[draw_start_screen] draws the start_screen when the record field game_state is
+  set to Start. This displays text asking for an input to start the game*)
 
-(*[draw_fail_screen] draws the fail_screen when the record field game_state
-   is set to Fail. *)
 val draw_fail_screen : unit -> unit
+(*[draw_fail_screen] draws the fail_screen when the record field game_state is
+  set to Fail. *)
 
-(*[draw_win_screen] draws the win_screen when the record field game_state
-   is set to Win. *)
 val draw_win_screen : unit -> unit
+(*[draw_win_screen] draws the win_screen when the record field game_state is set
+  to Win. *)
 
-(*[draw_pause_screen] draws the pause_screen when the record field game_state
-   is set to Pause. *)
 val draw_pause_screen : unit -> unit
+(*[draw_pause_screen] draws the pause_screen when the record field game_state is
+  set to Pause. *)
 
-(*[string_to_state s] converts a given s to type game_mode. It is used in 
-   [update_state s t]*)
 val string_to_state : string -> game_mode
+(*[string_to_state s] converts a given s to type game_mode. It is used in
+  [update_state s t]*)
 
-(*[update_state s t] changes the mutable record field game_state to whichever
-   game mode [s] represents. [s] is a string that needs to be converted to type
-   game_mode*)
 val update_state : string -> t -> unit
+(*[update_state s t] changes the mutable record field game_state to whichever
+  game mode [s] represents. [s] is a string that needs to be converted to type
+  game_mode*)
 
-(*[update_screen] checks the game state and executes the screen accordingly
-   
-For example, if game_state is equal to fail, it will draw the fail screen *)
 val update_screen : 'a -> t -> unit -> unit
+(*[update_screen] checks the game state and executes the screen accordingly. For
+  example, if game_state is equal to fail, it will draw the fail screen *)
